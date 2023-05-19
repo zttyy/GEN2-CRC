ede68b5b2442c58fa0177e24dab152213f1bee05
yzhou <61377585+zttyy@users.noreply.github.com>
2023-05-19 15:15:47 +0800

    Create crc.py

---
+++ crc.py
@@ -0,0 +1,23 @@
+def Crc5(datain, size):
+    polynomial = 0x9
+    crc5 = polynomial
+    if size > len(datain):
+        return 0xff
+    for i in range(size):
+        if ((crc5 >> 4) & 0x1) ^ datain[i]:
+            crc5 = ((crc5 << 1) ^ polynomial) & 0x1f
+        else:
+            crc5 = (crc5 << 1) & 0x1f
+    return crc5
+  
+def Crc16(datain, size):
+  polynomial = 0x1021
+  crc16 = 0xffff
+  if size > len(datain):
+      return 0xffff
+  for i in range(size):
+      if ((crc16 >> 15) & 0x1) ^ datain[i]:
+          crc16 = ((crc16 << 1) ^ polynomial) & 0xffff
+      else:
+          crc16 = (crc16 << 1) & 0xffff
+  return crc16 ^ 0xffff
